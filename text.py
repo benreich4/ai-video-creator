e6de4120f5098aaec85219753fe0fd3c2994ffdb
Ben Reich <benreich4@gmail.com>
2023-07-20 11:12:22 -0400

    Remove quotation marks and make sure the sentences are enumerated

--- text.py
+++ text.py
@@ -8,13 +8,13 @@ def gen(styleOf, topic):
 	response = openai.ChatCompletion.create(
 		model="gpt-3.5-turbo",
 		messages=[
-			{"role": "system", "content": f"Generate 4 short sentences in the style of {styleOf} on the topic of {topic}."},
+			{"role": "system", "content": f"Generate 3 enumerated short sentences in the style of {styleOf} on the topic of {topic}."},
 	])
 
 	msgs = response.choices[0].message.content.split("\n")
-	nonempty = [x for x in msgs if x]
+	nonempty = [x for x in msgs if x != ""]
 	for i in range(0,len(nonempty)):
-		#remove numbering
-		clean = msgs[i][3:]
+		#remove numbering and remove quotes
+		clean = msgs[i][3:].strip('"')
 		with open(f"{path(styleOf, topic)}/text/{i+1}.txt", 'w+') as f:
 				f.write(clean)
