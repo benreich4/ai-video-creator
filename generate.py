82f7959f13e59a67eb3713f7b6b296132b45053e
Ben Reich <benreich4@gmail.com>
2023-07-24 09:27:02 -0400

    Add helper functions easier to call from terminal

--- generate.py
+++ generate.py
@@ -15,14 +15,26 @@ def createPaths(styleOf, topic, length):
 	os.mkdir(f"{path}/audio")
 	os.mkdir(f"{path}/subtitles")
 	os.mkdir(f"{path}/video")
-
 	cmd = f"head -n {length} assets/list.txt > {path}/video/list.txt"
 	os.system(cmd)
 
-def gen(styleOf, topic, length):
-	createPaths(styleOf, topic, length)
-	text.gen(styleOf, topic, length)
+def genVideo(styleOf, topic, length):
+	video.genAll(styleOf, topic, length)
+
+def genAudio(styleOf, topic, length):
 	for i in range(1, length + 1):
 		audio.gen(styleOf, topic, i)
+
+def genImages(styleOf, topic, length):
+	for i in range(1, length + 1):
 		images.gen(styleOf, topic, i)
-	video.genAll(styleOf, topic, length)
+
+def genText(styleOf, topic, length):
+	text.gen(styleOf, topic, length)
+
+def gen(styleOf, topic, length):
+	createPaths(styleOf, topic, length)
+	genText(styleOf, topic, length)
+	genAudio(styleOf, topic, length)
+	genImages(styleOf, topic, length)
+	genVideo(styleOf, topic, length)
